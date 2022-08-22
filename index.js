ad938634f48223d7ed0831613fbdc381c62351dc
+ <+>
2022-08-22 18:58:01 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+
+function saturdayFun(fun="roller-skate!"){
+    console.log(`This Saturday, I want to ${fun}`);
+
+}
+saturdayFun();//returns default value,roller-skate!
+saturdayFun("bath my dog");//overrides default value
+
+const work=function mondayWork(work="go to the office"){
+console.log(`This Monday, I will ${work}`);
+}
+work();
+work("work from home");
+
+//wrapAdjective
+function wrapAdjective(){
+    
+}
 // code your solution here
